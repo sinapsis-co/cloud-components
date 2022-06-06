0a6ee5e6a2571287b6b02c9afcfeda40ddf79cb7
Andres Somadossi <andres@sinapsis.co>
2022-06-06 00:32:46 -0300

    platform-0.1.102

--- platform/integrations/bucket/index.ts
+++ platform/integrations/bucket/index.ts
@@ -1,23 +1,34 @@
 import S3 from 'aws-sdk/clients/s3';
 const s3 = new S3();
 
+const DEFAULT_MAX_LENGTH = 1024 * 1024 * 5; // 5 MB
+
+type PresignedPostParams = {
+  Bucket: string;
+  Key: string;
+  ContentType: string;
+  Expires?: number;
+  ContentLengthRange?: { min: number; max: number };
+};
+
+export const createPutPresignedUrl = (params: PresignedPostParams): S3.PresignedPost => {
+  const { Key, ContentType, ContentLengthRange, ...rest } = params;
+
+  return s3.createPresignedPost({
+    Expires: 3600,
+    ...rest,
+    Fields: {
+      key: Key,
+      'Content-Type': ContentType,
+    },
+    Conditions: [['content-length-range', ContentLengthRange?.min ?? 0, ContentLengthRange?.max ?? DEFAULT_MAX_LENGTH]],
+  });
+};
+
 export const createGetPresignedUrl = (bucket: string, key: string, expires = 3600): string => {
   return s3.getSignedUrl('getObject', {
     Bucket: bucket,
     Key: key,
     Expires: expires,
-  });
-};
-
-export const createPutPresignedUrl = (
-  bucket: string,
-  key: string,
-  contentType: string,
-  expires = 3600
-): S3.PresignedPost => {
-  return s3.createPresignedPost({
-    Bucket: bucket,
-    Expires: expires,
-    Fields: { key, 'Content-Type': contentType },
   });
 };
