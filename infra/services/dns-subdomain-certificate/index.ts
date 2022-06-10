b221cc238243e08e8142b7dbbc91542ddd5900ec
Andres Somadossi <andres@sinapsis.co>
2022-06-10 15:35:27 -0300

    infra-0.1.72

--- infra/services/dns-subdomain-certificate/index.ts
+++ infra/services/dns-subdomain-certificate/index.ts
@@ -1,23 +1,30 @@
 import { Construct } from 'constructs';
 import { HostedZone } from 'aws-cdk-lib/aws-route53';
-import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
+import { Certificate, CertificateValidation, ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 import { getDomain } from '../../common/naming/get-domain';
 import { Service } from '../../common/service';
 import { getLogicalName } from '../../common/naming/get-logical-name';
 
-export class DnsSubdomainCertificateConstruct extends Construct {
-  public readonly certificate: Certificate;
+export type DnsSubdomainCertificateConstructParams = {
+  certificateArn?: string;
+};
 
-  constructor(service: Service) {
+export class DnsSubdomainCertificateConstruct extends Construct {
+  public readonly certificate: ICertificate;
+
+  constructor(service: Service, params: DnsSubdomainCertificateConstructParams) {
     super(service, getLogicalName(DnsSubdomainCertificateConstruct.name));
 
-    const hostedZone = HostedZone.fromLookup(this, 'HostedZoneEnvDns', { domainName: getDomain('', service.props) });
-
-    this.certificate = new Certificate(this, 'Certificate', {
-      domainName: getDomain('', service.props),
-      subjectAlternativeNames: [getDomain('*', service.props)],
-      validation: CertificateValidation.fromDns(hostedZone),
-    });
+    if (params.certificateArn) {
+      this.certificate = Certificate.fromCertificateArn(this, 'HostedZoneEnvDns', params.certificateArn);
+    } else {
+      const hostedZone = HostedZone.fromLookup(this, 'HostedZoneEnvDns', { domainName: getDomain('', service.props) });
+      this.certificate = new Certificate(this, 'Certificate', {
+        domainName: getDomain('', service.props),
+        subjectAlternativeNames: [getDomain('*', service.props)],
+        validation: CertificateValidation.fromDns(hostedZone),
+      });
+    }
   }
 }
