95f2ce3a76a415c44d82c9e49a56e458a584d0f4
Andres Somadossi <andres@sinapsis.co>
2022-07-07 12:00:28 -0300

    infra-0.1.129

--- infra/script/index.ts
+++ infra/script/index.ts
@@ -4,6 +4,7 @@ import { deploySPA } from './deploy-spa';
 import { deploySSR } from './deploy-ssr';
 import { destroy } from './destroy';
 import { startSPA } from './start-spa';
+import { startSSR } from './start-ssr';
 
 export const invoke = (command: string): any => {
   const ops = {
@@ -12,7 +13,8 @@ export const invoke = (command: string): any => {
     destroy: destroy,
     'deploy-spa': deploySPA,
     'start-spa': startSPA,
-    'start-ssr': deploySSR,
+    'deploy-ssr': deploySSR,
+    'start-ssr': startSSR,
   };
   if (!ops[command]) throw new Error('Missing command');
   return ops[command];
