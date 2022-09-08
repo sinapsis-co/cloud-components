6fa12eddae3c9467cc527a355a3d79a07a0e3d59
Andres Somadossi <andres@sinapsis.co>
2022-09-08 23:33:21 +0200

    pipeline test

--- templates/services/support/deploy-pipeline/index.ts
+++ templates/services/support/deploy-pipeline/index.ts
@@ -8,9 +8,7 @@ export class DeployPipeline extends Service<GlobalProps> {
     super(scope, DeployPipeline.name, globalProps, { deployConfigName: 'deploy' });
 
     new DeployPipelineConstruct(this, {
-      preDeployCommands: [
-        'cd templates && yarn && yarn add -W @sinapsis-co/cc-infra-v2 && yarn add -W @sinapsis-co/cc-platform-v2 && echo "${$(cat config/index.ts)/isDemoProject: true,/isDemoProject: false,}" > config/index.ts',
-      ],
+      preDeployCommands: ['cd templates && bash pre-deploy.bash'],
       // postDeployCommands: [`yarn deploy-spa ${this.props.envName} webapp`],
     });
   }
