21fd09893b23ec38f5c04571499559ca5e672ce8
Andres Somadossi <andres@sinapsis.co>
2022-09-08 23:34:11 +0200

    pipeline test

--- templates/pre-deploy.bash
+++ templates/pre-deploy.bash
@@ -3,5 +3,5 @@ patten="isDemoProject: true,"
 sub="isDemoProject: false,"
 echo "${input/$patten/$sub}" >config/index.ts
 
-# yarn add -W @sinapsis-co/cc-infra-v2
-# yarn add -W @sinapsis-co/cc-platform-v2
+yarn add -W @sinapsis-co/cc-infra-v2
+yarn add -W @sinapsis-co/cc-platform-v2
