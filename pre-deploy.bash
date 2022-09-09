2262112a2698a29d3a7e2b80669d7e612cdd112b
Andres Somadossi <andres@sinapsis.co>
2022-09-09 11:47:53 +0200

    full deploy

--- pre-deploy.bash
+++ pre-deploy.bash
@@ -1,28 +1,28 @@
 # Deploy demo project
 
-msg=$(git log -1 --pretty=%B)
+# msg=$(git log -1 --pretty=%B)
 
-if [[ $msg == *"infra-"* || $msg == *"platform-"* ]]; then
-  echo "Skip"
-  exit
-fi
+# if [[ $msg == *"infra-"* || $msg == *"platform-"* ]]; then
+#   echo "Skip"
+#   exit
+# fi
 
-changes=$(git diff --dirstat=files,0 HEAD~1 | cut -d'%' -f2 | cut -d' ' -f2 | cut -d'/' -f1,2,3 | uniq)
+# changes=$(git diff --dirstat=files,0 HEAD~1 | cut -d'%' -f2 | cut -d' ' -f2 | cut -d'/' -f1,2,3 | uniq)
 
-hasInfraChanges=$(node -p "console.log(process.argv.slice(1).map(e => e.split('/')[0]).includes('infra'))" $changes)
-hasPlatformChanges=$(node -p "console.log(process.argv.slice(1).map(e => e.split('/')[0]).includes('platform'))" $changes)
+# hasInfraChanges=$(node -p "console.log(process.argv.slice(1).map(e => e.split('/')[0]).includes('infra'))" $changes)
+# hasPlatformChanges=$(node -p "console.log(process.argv.slice(1).map(e => e.split('/')[0]).includes('platform'))" $changes)
 
-if [[ $hasInfraChanges == *"true"* ]]; then
-  echo "Updating Infra package"
-  yarn release infra
-fi
+# if [[ $hasInfraChanges == *"true"* ]]; then
+#   echo "Updating Infra package"
+#   yarn release infra
+# fi
 
-if [[ $hasInfraChanges == *"true"* ]]; then
-  echo "Updating Platform package"
-  yarn release platform
-fi
+# if [[ $hasInfraChanges == *"true"* ]]; then
+#   echo "Updating Platform package"
+#   yarn release platform
+# fi
 
-git push origin dev
+# git push origin dev
 
 cd templates
 
--- release.bash
+++ release.bash
@@ -5,12 +5,11 @@ version=$(grep -n "version" package.json | cut -d':' -f3 | cut -d'"' -f2)
 inm=$(echo $version | cut -d'.' -f1 -f2)
 pre=$(echo $version | cut -d'.' -f3)
 current=$((pre + 1))
-echo $current
 yarn build
 cp package.json .dist/package.json
 cd .dist
 yarn publish --new-version $inm.$current --no-git-tag-version
 cp package.json ../package.json
 cd ..
-git add . && git commit -m "$package-$inm.$current"
+git add . && git commit -m "$package-$inm.$current" && git push origin dev
 cd ..
