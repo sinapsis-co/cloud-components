01e7ebf2e00436b61c8ee2dda1ecbebd4ef79c79
Andres Somadossi <andres@sinapsis.co>
2023-03-01 12:54:47 +0530

    infra-0.1.165

--- infra/prefab/config/parameter-secret.ts
+++ infra/prefab/config/parameter-secret.ts
@@ -1,24 +1,34 @@
-import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { IStringParameter, StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
 
 import { getLogicalName } from '../../common/naming/get-logical-name';
 import { getParameterNamePlain } from '../../common/naming/get-resource-name';
 import { Service } from '../../common/service';
+import { SynthError } from '../../common/synth/synth-error';
 
 export type ParameterSecretProps = {
-  name: string;
+  name?: string;
+  existingName?: string;
 };
 
 export class ParameterSecret extends Construct {
-  public readonly secret: StringParameter;
+  public readonly secret: IStringParameter;
 
   constructor(service: Service, params: ParameterSecretProps) {
     super(service, getLogicalName(ParameterSecret.name, params.name));
 
-    this.secret = new StringParameter(this, getLogicalName('param', params.name), {
-      simpleName: false,
-      parameterName: getParameterNamePlain(params.name, service.props),
-      stringValue: 'Default value: this need to be changed',
-    });
+    if (params.existingName || params.name) throw new SynthError('name or existingName are required');
+
+    if (params.existingName) {
+      this.secret = StringParameter.fromStringParameterName(this, getLogicalName('param', params.name), params.name);
+    }
+
+    if (params.name) {
+      this.secret = new StringParameter(this, getLogicalName('param', params.name), {
+        simpleName: false,
+        parameterName: getParameterNamePlain(params.name, service.props),
+        stringValue: 'Default value: this need to be changed',
+      });
+    }
   }
 }
