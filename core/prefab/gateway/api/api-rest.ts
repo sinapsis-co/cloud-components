39d8bfe8b947a55380fcd7795107e9a5241ae6fd
Andres Somadossi <andres@sinapsis.co>
2023-04-27 00:26:07 +0700

    core-3.0.31

--- core/prefab/gateway/api/api-rest.ts
+++ core/prefab/gateway/api/api-rest.ts
@@ -64,9 +64,7 @@ export class ApiRestPrefab extends Construct {
     this.basePath = this.api.root.resourceForPath(params.basePath);
 
     if (params.httpProxyIntegrationUrl) {
-      this.basePath
-        .addProxy({ anyMethod: false })
-        .addMethod('ANY', new awsApigateway.HttpIntegration(params.httpProxyIntegrationUrl));
+      this.basePath.addProxy({ defaultIntegration: new awsApigateway.HttpIntegration(params.httpProxyIntegrationUrl) });
     }
 
     if (params.cdnApiPrefab)
