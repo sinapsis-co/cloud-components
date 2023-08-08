93d28bb8e0f8b3b1e9e469e81f75e6f21303a7b4
Andres Somadossi <andres@sinapsis.co>
2023-08-08 18:47:15 -0300

    sdk-3.0.28

--- sdk/integration/store/dynamo/client.ts
+++ sdk/integration/store/dynamo/client.ts
@@ -3,4 +3,4 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { Tracing } from 'tracing';
 
 const client: DynamoDBClient = Tracing.captureIntegration(new DynamoDBClient({}) as any);
-export const dynamodb = DynamoDBDocumentClient.from(client);
+export const dynamodb = DynamoDBDocumentClient.from(client, { marshallOptions: { removeUndefinedValues: true } });
