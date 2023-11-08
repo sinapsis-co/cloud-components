35ec6b7585253bca1cb6492b74c97fb8336a0553
Hernan Sinapsis <hernan@sinapsis.co>
2023-11-08 14:01:31 -0300

    WIP

--- frontend/spa-webapp/src/components/fullscreen-loading/index.tsx
+++ frontend/spa-webapp/src/components/fullscreen-loading/index.tsx
@@ -9,48 +9,83 @@ import { alpha, styled } from '@mui/material/styles';
 
 import Isologo from '../isologo';
 
+// Framer Motion configuration for the backdrop animation
+const backdropMotionConfig = {
+  initial: { opacity: 0, y: 0 },
+  animate: {
+    opacity: [0, 1],
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ...easing,
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: '-25%',
+    transition: {
+      duration: 0.25,
+      ...easing,
+    },
+  },
+};
+
 const Container = styled('div')(() => ({
   isolation: 'isolate',
   position: 'absolute',
-  top: 0,
-  left: 0,
-  width: '100vw',
-  height: '100dvh',
+  width: '100%',
+  height: '100%',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   cursor: 'wait',
+  zIndex: 1000,
+  pointerEvents: 'none',
+
+  '&[data-fullscreen="true"]': {
+    position: 'fixed',
+    width: '100vw',
+    height: '100dvh',
+    inset: 0,
+    transform: 'none',
+  },
 
   '& .wrapper-isologo': {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    zIndex: 99999,
+    zIndex: 3000,
+    pointerEvents: 'none',
     '& .isologo': {
       width: 'max(5vw, 150px)',
       height: 'max(5vw, 150px)',
-      aspectRatio: '1/1',
+      aspectRatio: 1,
     },
   },
 }));
 
-const Backdrop = styled(motion.span)(({ theme }) => ({
+const Backdrop = styled(motion.div)(({ theme }) => ({
+  display: 'block',
   position: 'absolute',
   top: 0,
   left: 0,
   width: '100%',
   height: '100%',
   backgroundColor: alpha(theme.palette.background.default, 0.5),
-  zIndex: theme.zIndex.modal,
+  zIndex: 2000,
   backdropFilter: `blur(${theme.spacing(0.5)})`,
   WebkitBackdropFilter: `blur(${theme.spacing(0.5)})`,
+  pointerEvents: 'none',
 }));
 
-// Define the properties for the FullscreenLoading component
 interface FullscreenLoadingProps {
   className?: string;
-  fullscreen?: boolean;
+  isFullscreen?: boolean;
+  hasBackdrop?: boolean;
 }
 
 /**
@@ -59,62 +94,47 @@ interface FullscreenLoadingProps {
  * @component
  * @param {Object} props - The component's properties.
  * @param {string} [props.className] - Additional CSS classes for styling.
- * @param {boolean} [props.fullscreen] - Determines whether the loading indicator is full-screen.
+ * @param {boolean} [props.isFullscreen] - Determines whether the loading indicator is full-screen.
+ * @param {boolean} [props.hasBackdrop] - Determines whether the loading indicator has a backdrop.
  */
-const FullscreenLoading: FunctionComponent<FullscreenLoadingProps> = ({ className, fullscreen = true }) => {
-  const shouldReduceMotion = useReducedMotion(); // Check for reduced motion preference
+const FullscreenLoading: FunctionComponent<FullscreenLoadingProps> = ({ className, isFullscreen, hasBackdrop }) => {
+  const shouldReduceMotion = useReducedMotion();
   const { formatMessage } = useIntl();
 
   return (
     <Container
       id="loading"
       className={className || ''}
-      data-fullscreen={fullscreen}
-      aria-busy="true" // Indicate that the page is loading
-      role="status" // Add ARIA role to indicate a status message
-      aria-label={formatMessage({ id: 'COMMON.LOADING' })} // Provide an ARIA label to describe the loading process
+      data-fullscreen={isFullscreen}
+      aria-busy="true" // Indicates that the page is loading
+      role="status" // Indicates a status message
+      aria-label={formatMessage({ id: 'COMMON.LOADING' })} // Describes the loading process
+      onClick={(event) => event.stopPropagation()}
     >
-      {/* This wrapper is used solely for positioning purposes, and it won't interfere with Framer Motion's transform animations. */}
       <div className="wrapper-isologo">
         <Isologo className="isologo" />
       </div>
-      <Backdrop
-        initial={shouldReduceMotion ? {} : { opacity: 0, y: 0 }}
-        animate={
-          shouldReduceMotion
-            ? {}
-            : {
-                opacity: 1,
-                y: 0,
-                transition: {
-                  duration: 0.5,
-                  ...easing,
-                },
-              }
-        }
-        exit={
-          shouldReduceMotion
-            ? {}
-            : {
-                opacity: 0,
-                y: '-25%',
-                transition: {
-                  duration: 0.25,
-                  ...easing,
-                },
-              }
-        }
-      />
+      {(isFullscreen || hasBackdrop) && (
+        <Backdrop
+          role="presentation" // Indicates a presentation
+          aria-hidden="true" // Hides the backdrop from screen readers
+          aria-label={formatMessage({ id: 'COMMON.LOADING' })} // Describes the loading process
+          aria-describedby="loading" // References the loading indicator
+          aria-atomic="true" // Indicates that the loading indicator is atomic
+          aria-live="polite" // Indicates that the loading indicator is polite
+          aria-busy="true" // Indicates that the page is loading
+          onClick={(event) => event.stopPropagation()}
+          {...(shouldReduceMotion ? {} : backdropMotionConfig)}
+        />
+      )}
     </Container>
   );
 };
 
-/**
- * Default props for the `FullscreenLoading` component.
- */
 FullscreenLoading.defaultProps = {
   className: '',
-  fullscreen: true,
+  isFullscreen: true,
+  hasBackdrop: true,
 };
 
 export default FullscreenLoading;
--- frontend/spa-webapp/src/pages/index.tsx
+++ frontend/spa-webapp/src/pages/index.tsx
@@ -1,12 +1,25 @@
-import { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 
 import { AutocompleteOption } from '@webapp/components/form/autocomplete';
+import FullscreenLoading from '@webapp/components/fullscreen-loading';
 import IconAutocomplete from '@webapp/components/icon-autocomplete';
 import { icons } from '@webapp/components/icon-autocomplete/icons';
 import ThemeToggle from '@webapp/components/theme-toggle';
 
 const HomePage: FunctionComponent = () => {
-  const [selectedIcon, setSelectedIcon] = useState<AutocompleteOption>();
+  const [selectedIcon, setSelectedIcon] = React.useState<AutocompleteOption>();
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div
       style={{
@@ -17,15 +30,12 @@ const HomePage: FunctionComponent = () => {
         gap: '1rem',
         width: '100vw',
         height: '100vh',
+        position: 'relative',
       }}
     >
       <ThemeToggle />
-      <IconAutocomplete
-        iconList={icons}
-        onSelect={(selectedIcon) => {
-          setSelectedIcon(selectedIcon);
-        }}
-      />
+      <IconAutocomplete iconList={icons} onSelect={() => setSelectedIcon(selectedIcon)} />
+      {loading ? <FullscreenLoading isFullscreen /> : null}
     </div>
   );
 };
